eb78a694ba0326d7750c76eb5adca14b215f0392
Prashant <hadaprashant2002@gmail.com>
2025-02-05 16:08:06 +0530

    added CTA and other minor changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,71 +1,73 @@
-'use client'
-import Link from 'next/link';
-import {useRef, useEffect} from 'react'
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+"use client";
+import Link from "next/link";
+import { useRef, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 const HeroSection = () => {
   const imageRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const imageElement = imageRef.current;
+    const handleScroll = () => {
+      if (!imageRef.current) return;
 
-    const handleScroll =()=>{
       const scrollPosition = window.scrollY;
-    const scrollThreshold = 100;
+      const scrollThreshold = 100;
 
-      if(scrollPosition > scrollThreshold){
-        imageElement.classList.add('scrolled');
+      if (scrollPosition > scrollThreshold) {
+        imageRef?.current.classList.add("scrolled");
+      } else {
+        imageRef?.current.classList.remove("scrolled");
       }
-      else{
-        imageElement.classList.remove('scrolled');
-      }
-    }
+    };
 
-    window.addEventListener('scroll', handleScroll); 
-  
-    return()=> window.removeEventListener('scroll', handleScroll); 
-  }, [])
-  
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <section className=' z-10 pt-36 md:pt-42 pb-10'>
-        <div className=" space-y-6 text-center flex flex-col items-center justify-center">
-          <div className=" space-y-6 mx-auto flex flex-col items-center justify-center">
-            <h1 className='text-3xl font-bold md:text-5xl lg:text-7xl gradient-title'>
-              Your Ai Career Coach for
-              <br />
-              Professional Success
-            </h1>
-            <p 
-            className='mx-auto max-w[600px] text-muted-foreground md:text-xl md:mx-[450px]'
-            >
-              Advance your career with personalized guidance, interview prep, and AI-powered tools for jobs success.
-            </p>
-            <div className=" flex items-center gap-4">
-              <Link href={'/dashboard'}>
-                <Button size={'lg'} className='px-8'>Get Started</Button>
-              </Link>
-              <Link href={'/dashboard'}>
-                <Button size={'lg'} className='px-8' variant={'outline'}>Watch Demo</Button>
-              </Link>
-            </div>
+    <section className=" z-10 pt-36 md:pt-42 pb-10">
+      <div className=" space-y-6 text-center flex flex-col items-center justify-center">
+        <div className=" space-y-6 mx-auto flex flex-col items-center justify-center">
+          <h1 className="text-3xl font-bold md:text-5xl lg:text-7xl gradient-title">
+            Your Ai Career Coach for
+            <br />
+            Professional Success
+          </h1>
+          <p className="mx-auto max-w[600px] text-muted-foreground md:text-xl md:mx-[450px]">
+            Advance your career with personalized guidance, interview prep, and
+            AI-powered tools for jobs success.
+          </p>
+          <div className=" flex items-center gap-4">
+            <Link href={"/dashboard"}>
+              <Button size={"lg"} className="px-8">
+                Get Started
+              </Button>
+            </Link>
+            <Link href={"/dashboard"}>
+              <Button size={"lg"} className="px-8" variant={"outline"}>
+                Watch Demo
+              </Button>
+            </Link>
+          </div>
 
-            <div className="hero-image-wrapper mt-5 md:mt-0">
-              <div ref = {imageRef} className="hero-image">
-                <Image 
-                src='/banner3.jpeg'
+          <div className="hero-image-wrapper mt-5 md:mt-0">
+            <div ref={imageRef} className="hero-image">
+              <Image
+                src="/banner3.jpeg"
                 width={1080}
                 height={720}
-                alt='Banner Image'
-                className='rounded-lg shadow-2xl border mx-auto'
-                priority />
-              </div>
+                alt="Banner Image"
+                className="rounded-lg shadow-2xl border mx-auto"
+                priority
+              />
             </div>
           </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default HeroSection;
