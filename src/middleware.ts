36e39c9f115c1e635881ce13ee534ccf1b0762ce
Prashant <hadaprashant2002@gmail.com>
2025-02-12 20:40:55 +0530

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,6 +10,17 @@ const protectedRoutes = createRouteMatcher([
 ])
 export default clerkMiddleware(async(auth, req )=>{
   const {userId} = await auth();
+  const url = req.nextUrl;
+
+  // Redirect for the root route '/'
+  if (url.pathname === '/') {
+    // If not logged in, redirect to /landing
+    if (!userId) {
+      return NextResponse.redirect(new URL('/landing', req.url));
+    }
+    // If logged in, redirect to /dashboard
+    return NextResponse.redirect(new URL('/dashboard', req.url));
+  }
 
   if(!userId && protectedRoutes(req)){
     const {redirectToSignIn} = await auth();
