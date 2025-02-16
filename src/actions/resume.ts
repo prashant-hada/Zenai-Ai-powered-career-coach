366ef1952d7fe0ac086920854ca383fcedf690ca
Prashant <hadaprashant2002@gmail.com>
2025-02-16 21:57:07 +0530

    added server actions for resume feature

---
+++ src/actions/resume.ts
@@ -0,0 +1,111 @@
+'use server'
+
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server"
+import { revalidatePath } from "next/cache";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+export async function getResume() {
+    try {
+        const {userId} = await auth();
+        if (!userId) throw new Error("Unauthorized");
+
+        const user = await db.user.findUnique({
+            where:{
+                clerkUserId:userId
+            }
+        });
+        if (!user) throw new Error("User Not Found");
+
+        const resume= await db.resume.findUnique({
+            where:{
+                userId: user.id
+            }
+        })
+
+        if (!resume) throw new Error("Resume Not Found");
+
+        return resume;
+    } catch (error) {
+        console.error("Error Fetching resume:", error);
+    throw new Error("Failed to Fetch resume : " + (error as Error).message);
+    }
+}
+
+export async function saveResume(content: string) {
+    try {
+        const {userId} = await auth();
+        if (!userId) throw new Error("Unauthorized");
+
+        const user = await db.user.findUnique({
+            where:{
+                clerkUserId:userId
+            }
+        });
+        if (!user) throw new Error("User Not Found");
+
+        const resume = await db.resume.upsert({
+            where:{
+                userId:user.id
+            },
+            update:{
+                content:content
+            },
+            create:{
+                userId:user.id,
+                content:content
+            }
+        })
+
+        revalidatePath("/resume");
+    return resume;
+  } catch (error) {
+    console.error("Error saving resume:", error);
+    throw new Error("Failed to save resume");
+  }
+
+}
+
+export async function improveWithAi(current:string, type:string) {
+    try {
+        const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+  
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+      include: {
+        industryInsight: true,
+      },
+    });
+  
+    if (!user) throw new Error("User not found");
+    
+    const prompt = `
+    As an expert resume writer, improve the following ${type} description for a ${user.industry} professional.
+    Make it more impactful, quantifiable, and aligned with industry standards.
+    Current content: "${current}"
+
+    Requirements:
+    1. Use action verbs
+    2. Include metrics and results where possible
+    3. Highlight relevant technical skills
+    4. Keep it concise but detailed
+    5. Focus on achievements over responsibilities
+    6. Use industry-specific keywords
+    
+    Format the response as a single paragraph without any additional text or explanations.
+  `;
+  const result = await model.generateContent(prompt);
+  const response = result.response;
+  const improvedContent = response.text().trim();
+  return improvedContent;
+} catch (error) {
+  console.error("Error improving content:", error);
+  throw new Error("Failed to improve content");
+}
+}
--- src/app/(auth)/sign-in/[[...rest]]/page.tsx
+++ src/app/(auth)/sign-in/[[...rest]]/page.tsx
@@ -2,7 +2,8 @@ import { SignIn } from '@clerk/nextjs'
 import React from 'react'
 
 const Page = () => {
-  return <SignIn signUpFallbackRedirectUrl={process.env.SIGN_IN_REDIRECT_URL} />
+  // return <SignIn forceRedirectUrl={process.env.SIGN_IN_REDIRECT_URL} />
+  return <SignIn />
 }
 
 export default Page
