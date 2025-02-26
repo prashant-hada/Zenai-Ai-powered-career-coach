c008d5b6a5291f0e1528eb80b184942a8fcd219e
Prashant <hadaprashant2002@gmail.com>
2025-02-27 00:27:49 +0530

    worked on creating server action for cover letter feature

---
+++ src/types/CoverLetter.ts
@@ -0,0 +1,9 @@
+export default interface CoverLetter {
+  userId: string;
+  content: string;
+  jobDescription: string;
+  companyName: string;
+  jobTitle: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
